0a9ed6de1a28143802db5489208ddd193f213963
Juan Ketterhagen <jketterj@gmail.com>
2015-10-29 11:54:38 -0500

    subcategory posts show for parent category, added ratings to posts' upvote arrays

--- client/templates/category/categoryPage.js
+++ client/templates/category/categoryPage.js
@@ -2,9 +2,25 @@ Template.categoryPage.helpers({
     //Return the posts to be displayed
     'posts': function() {
         if (Template.instance().data.type == "category") {
-            return Posts.find({
-                'categoryID': Template.instance().data.object._id,
+            var cats = [Template.instance().data.object._id],
+                subCats = Categories.find({parentID: Template.instance().data.object._id});            
+            
+            subCats.forEach(function(obj){
+                cats.push(obj._id);
             });
+
+            return Posts.find({
+                'categoryID': {
+                    '$in': cats
+                }
+            });
+            
+            
+            //Below line is all that's needed if the correct posts are published (currently true)
+            /*
+            return Posts.find();
+            */
+
         } else if (Template.instance().data.type == "tag") {
             var postIDs = [];
             Post_tags.find({'tag': Template.instance().data.object }).forEach(function(doc) {
